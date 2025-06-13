b5de75c1b26a13e054f5c5b22d7ab1220c18e065
Chris LaFumee <chrislafumee@gmail.com>
2025-06-13 17:09:41 -0400

    initial styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Rewards from './pages/Rewards/Rewards';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-950 text-white">
+    <div className="min-h-screen bg-black text-gray-200">
       <Header />
       <main className="p-6 text-center">
              <Routes>
@@ -16,7 +16,7 @@ function App() {
           <Route path="/habits" element={<Habits />} />
           <Route path="/rewards" element={<Rewards />} />
         </Routes>
-        <h1 className="text-4xl font-bold">Limitless Progressions</h1>
+        <h1 className="text-4xl font-bold mb-4 py-4">Limitless Progressions</h1>
         <p className="text-gray-400 mt-2">Track habits. Stay consistent. Earn rewards.</p>
       </main>
     </div>
