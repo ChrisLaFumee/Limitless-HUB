0e45da15c6b147ad7e384a86ac6ff2a6713069c8
Chris LaFumee <chrislafumee@gmail.com>
2026-02-04 17:55:49 -0500

    coaching update

--- src/components/Coaching/Coaching.tsx
+++ src/components/Coaching/Coaching.tsx
@@ -15,7 +15,7 @@ const Coaching = () => {
           tailored to your goals, lifestyle, and hustle.
         </p>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* Virtual Coaching */}
           <div
             onClick={() =>
